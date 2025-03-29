6fd45dc23fea0871a154c5a59f153ad306cbedee
hiumonday <hieunt04lm@gmail.com>
2025-03-29 12:49:39 +0000

    login

--- crates/db/migrations/20250319060359_user_tables.sql
+++ crates/db/migrations/20250319060359_user_tables.sql
@@ -2,8 +2,21 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     email VARCHAR NOT NULL UNIQUE, 
+    hashed_password VARCHAR,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    session_verifier VARCHAR NOT NULL,
+    otp_code_encrypted VARCHAR,
+    otp_code_confirmed BOOLEAN DEFAULT FALSE,
+    otp_code_attempts INTEGER DEFAULT 0,
+    otp_code_sent TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMP NOT NULL DEFAULT NOW() + interval '30 days'
 );
 
 INSERT INTO users(email) VALUES('test1@test1.com');
@@ -11,4 +24,5 @@ INSERT INTO users(email) VALUES('test2@test1.com');
 INSERT INTO users(email) VALUES('test3@test1.com');
 
 -- migrate:down
-DROP TABLE users;
+DROP TABLE sessions; 
+DROP TABLE users;    
